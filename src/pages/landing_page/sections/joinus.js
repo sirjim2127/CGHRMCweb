d85b99d886e07bce8ec1825984860fec15ab1b28
jcbsit2k19 <johnchristianalvarezbsit2k19@gmail.com>
2024-05-19 17:26:08 +0800

    join us link

--- src/pages/landing_page/sections/joinus.js
+++ src/pages/landing_page/sections/joinus.js
@@ -10,7 +10,7 @@ export default function JoinUsSection() {
         {/* <p className='text-lg text-white mb-8'>Make a difference, protect lives, safeguard our seas, and serve your country. Join the Coast Guard today!</p> */}
         <button
           onClick={() => {
-            window.open("https://facebook.com", "_blank");
+            window.open("https://cg-hrmc.com/", "_blank");
           }}
           className="px-6 py-3 border-2 rounded-full bg-gradient-to-r from-blue-800 to-blue-600 text-xl text-white font-semibold shadow-md hover:scale-105 transform transition duration-300">
           Apply Now
