b4785ef70af859cee3eb89178ae23c6f0de6803d
JC Alvarez <johnchristianalvarezbsit2k19@gmail.com>
2024-08-27 14:54:37 +0800

    fillup

---
+++ src/pages/osm_page/components/fillup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function FillUp() {
+    return (
+
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeIdMaJpP6EEFEA33KSDLRfq68Ogz5ZmT7TReNFmyubOwrkRw/viewform?embedded=true"
+            className='w-full h-full'
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            title="form"
+        >Loading…</iframe>
+
+    );
+}
--- src/pages/osm_page/index.js
+++ src/pages/osm_page/index.js
@@ -3,6 +3,7 @@ import Textbox from "../../components/textbox/textbox";
 import Button from "../../components/button/button";
 import Swal from "sweetalert2";
 import { Link } from "react-router-dom";
+import FillUp from "./components/fillup";
 
 export default function OSM() {
   const [accessPage, setAccessPage] = useState("signin");
@@ -20,84 +21,91 @@ export default function OSM() {
   };
   if (accessPage === "signin")
     return (
-      <div className="max-w-lg w-full h-full mx-auto py-8 px-4">
-        <div
-          style={{
-            boxShadow:
-              "0 10px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
-          }}
-          className="bg-white rounded-lg shadow-xl overflow-hidden border">
-          <div className="p-8">
-            <h2 className="text-center text-3xl font-extrabold text-gray-700">
-              Welcome Back
-            </h2>
-            <p className="mt-4 text-center text-gray-400">
-              Sign in to continue
-            </p>
-            <form
-              method="POST"
-              action="#"
-              className="mt-8 space-y-6"
-              onSubmit={handleSubmit}>
-              <div className="rounded-md shadow-sm space-y-4">
-                <Textbox
-                  required
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email address"
-                  value={formData?.email}
-                  onChange={(value) =>
-                    setFormData({ ...formData, email: value })
-                  }
-                />
-                <Textbox
-                  required
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                  value={formData?.password}
-                  onChange={(value) =>
-                    setFormData({ ...formData, password: value })
-                  }
-                />
-              </div>
-
-              <div className="flex items-center justify-between mt-4">
-                <div className="flex items-center">
-                  <input
-                    className="h-4 w-4 text-indigo-500 focus:ring-indigo-400 border-gray-600 rounded"
-                    type="checkbox"
-                    name="remember-me"
-                    id="remember-me"
+      <div className="w-full h-full mx-auto py-8 px-4">
+        <div className="h-full  md:flex md:space-y-0 space-y-2 space-x-0 md:space-x-2">
+          <div
+            style={{
+              boxShadow:
+                "0 10px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+            }}
+            className="bg-white rounded-lg shadow-xl flex-1">
+            <div className="p-8">
+              <h2 className="text-center text-3xl font-extrabold text-gray-700">
+                Welcome Back
+              </h2>
+              <p className="mt-4 text-center text-gray-400">
+                Sign in to continue
+              </p>
+              <form
+                method="POST"
+                action="#"
+                className="mt-8 space-y-6"
+                onSubmit={handleSubmit}>
+                <div className="rounded-md shadow-sm space-y-4">
+                  <Textbox
+                    required
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email address"
+                    value={formData?.email}
+                    onChange={(value) =>
+                      setFormData({ ...formData, email: value })
+                    }
                   />
-                  <label
-                    className="ml-2 block text-sm text-gray-400"
-                    htmlFor="remember-me">
-                    Remember me
-                  </label>
+                  <Textbox
+                    required
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    value={formData?.password}
+                    onChange={(value) =>
+                      setFormData({ ...formData, password: value })
+                    }
+                  />
                 </div>
 
-                <div className="text-sm">
-                  <span className="font-medium text-indigo-500 hover:text-indigo-400">
-                    Forgot your password?
-                  </span>
-                </div>
-              </div>
+                <div className="flex items-center justify-between mt-4">
+                  <div className="flex items-center">
+                    <input
+                      className="h-4 w-4 text-indigo-500 focus:ring-indigo-400 border-gray-600 rounded"
+                      type="checkbox"
+                      name="remember-me"
+                      id="remember-me"
+                    />
+                    <label
+                      className="ml-2 block text-sm text-gray-400"
+                      htmlFor="remember-me">
+                      Remember me
+                    </label>
+                  </div>
 
-              <Button type={"submit"}>Sign In</Button>
-            </form>
+                  <div className="text-sm">
+                    <span className="font-medium text-indigo-500 hover:text-indigo-400">
+                      Forgot your password?
+                    </span>
+                  </div>
+                </div>
+
+                <Button type={"submit"}>Sign In</Button>
+              </form>
+            </div>
+            <div className="px-8 py-4 bg-gray-300 text-center">
+              <span className="text-gray-400">Don't have an account?</span>
+              <Link
+                className="font-medium text-indigo-500 hover:text-indigo-400"
+                to={"/sign-up"}>
+                Sign up
+              </Link>
+            </div>
           </div>
-          <div className="px-8 py-4 bg-gray-300 text-center">
-            <span className="text-gray-400">Don't have an account?</span>
-            <Link
-              className="font-medium text-indigo-500 hover:text-indigo-400"
-              to={"/sign-up"}>
-              Sign up
-            </Link>
+          <div className="flex-1 rounded-md shadow-md">
+            <FillUp />
           </div>
+
         </div>
+
       </div>
     );
 
